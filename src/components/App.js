ae9811b6643e0137644b14276be92f054574db10
osbornetunde <osbornetunde@gmail.com>
2019-02-20 12:40:23 +0100

    feature(search functionality): -installed axios for fetching data from youtube API -connected the app to youtube api -returns the number of videos found [Delivers #164102818]

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube'
 
 
 class App extends React.Component {
+    state = {videos: [] };
 
+
+    handelTermSubmit = async term => {
+          const response = await  youtube.get('/search', {
+                params: {
+                    q: term
+                }
+            });
+
+        this.setState({ videos: response.data.items });
+    };
 
     render() {
         return (
         <div className="container">
-            <SearchBar />
+            <SearchBar onFormSubmit={this.handelTermSubmit}/>
+            I have found {this.state.videos.length} videos.
         </div>
         )}
 }
