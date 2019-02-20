8b228cb1791a4a01c8656ddc96a4197703721a8b
osbornetunde <osbornetunde@gmail.com>
2019-02-20 13:53:37 +0100

    ft(styling video list): -properly aligned the video image and description using custom CSS and semantic UI [Delivers #164102852]

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './VideoItem.css'
 
 const VideoItem = ({ video })=> {
-    const { id, snippet } = video
+    const { snippet } = video
     return (
-    <div key={id.videoId}>
-    <img alt={snippet.description} src={snippet.thumbnails.medium.url} />
-    {snippet.title}
+    <div className="video-item item">
+    <img className="ui image" alt={snippet.description} src={snippet.thumbnails.medium.url} />
+    <div className="content">
+        <div className="header">{snippet.title}</div>
+    </div>
     </div>
     )
 }
