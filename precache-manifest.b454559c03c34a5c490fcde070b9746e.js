7a538460bbe21ad0b60381d537813781498266d5
osbornetunde <osbornetunde@gmail.com>
2019-02-20 15:47:31 +0100

    Updates

---
+++ precache-manifest.b454559c03c34a5c490fcde070b9746e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aa4bba83bbe54ee5583",
+    "url": "/videoSearch/static/js/runtime~main.6aa4bba8.js"
+  },
+  {
+    "revision": "1be34de1b7d391a9d5b7",
+    "url": "/videoSearch/static/js/main.1be34de1.chunk.js"
+  },
+  {
+    "revision": "5918dbad8567dc79d88f",
+    "url": "/videoSearch/static/js/2.5918dbad.chunk.js"
+  },
+  {
+    "revision": "1be34de1b7d391a9d5b7",
+    "url": "/videoSearch/static/css/main.930021bd.chunk.css"
+  },
+  {
+    "revision": "359f8351509fafbeadb8ddd3433efb7c",
+    "url": "/videoSearch/index.html"
+  }
+];
